1d86521c714bd25f7d65c75b013b8bfd2c414784
Priscila Padilla Latance <88410500+priscilalatance@users.noreply.github.com>
2022-02-13 12:46:48 -0300

    Add files via upload

---
+++ myApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+               Console.WriteLine($"Bem Vindo ao curso de .NET 5 ({i})"); 
+            }
+            
+        }
+    }
+}
